a9e9e5e1a9c5b1ed5154da9853a001e72744a1a8
Devorris Russell <devorris_r@yahoo.com>
2021-11-23 15:51:23 -0500

    adding files to do mongo

---
+++ index.js
@@ -0,0 +1,16 @@
+const connectDB = require('./startup/db');
+const express = require('express');
+const app = express();
+const products = require('./routes/products');
+
+connectDB();
+
+
+app.use(express.json());
+const port = process.env.PORT || 5000;
+app.use('/api/products', products);
+
+
+app.listen(port,() => {
+    console.log('Server started on port: ${port}');
+});
---
+++ routes/products.js
@@ -0,0 +1,27 @@
+const Product = require('../models/product');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', async (req, res) => {
+    try {
+        const product = new Product({
+            name: 'Stanley Classic Vaccum Bottle',
+            description: 'Our Stanley Classic Vacuum Bottle is made with superior insulation that keeps liquid (soup, coffe, tea) hot or cold drinks cool for up to 24 hours.',
+category: 'Travel',
+price: 19.82,
+        });
+        await product.save();
+        return res.send(product);
+    } catch (ex) {
+        return res.status(500).send('Internal Server Error: ${ex}');
+    }
+});
+
+
+
+
+
+
+
+module.exports = router;
